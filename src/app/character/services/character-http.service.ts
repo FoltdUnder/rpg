2f208bf9c199cfcdcb249ded269e56e705b3f072
foltdUnder <twinker7130@gmail.com>
2022-04-24 20:15:43 +0300

    feat(http-service): add http service for character builder

--- src/app/character/services/character-http.service.ts
+++ src/app/character/services/character-http.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from 'rxjs';
-import {CharacterBuilder} from '../../character.model';
+import {CharacterBuilder} from '../character.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CharacterBuilderHttpService {
+export class CharacterHttpService {
 
   constructor(private httpClient: HttpClient) { }
 
