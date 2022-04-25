8972ae14e1ebb48ca74099425c0636ea71173e60
foltdUnder <twinker7130@gmail.com>
2022-04-25 11:37:07 +0300

    feat(menu): show menu elements in sign in condition

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {isLoggedIn, isLoggedOut, selectAuthState} from '../../auth/auth.selectors';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  isLoggedOut$ = new Observable<boolean>()
+  isLoggedIn$ = new Observable<boolean>()
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.isLoggedOut$ = this.store
+      .pipe(
+        select(isLoggedOut)
+      );
+    this.isLoggedIn$ = this.store
+      .pipe(
+        select(isLoggedIn)
+      );
   }
 
 }
