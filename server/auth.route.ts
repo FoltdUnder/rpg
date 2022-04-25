9b84de63cbaf5aa3f01c0d18195a76edd5ab927a
foltdUnder <twinker7130@gmail.com>
2022-04-25 03:16:48 +0300

    feat(server): add login method

---
+++ server/auth.route.ts
@@ -0,0 +1,18 @@
+import {authenticate} from './db-data';
+import {Request, Response} from 'express';
+
+
+export function loginUser(req: Request, res: Response) {
+
+  console.log("User login attempt ...");
+  const {login, password} = req.body;
+  const user = authenticate(login, password);
+
+  if (user) {
+    res.status(200).json({id:user.id, login: user.login});
+  }
+  else {
+    res.sendStatus(403);
+  }
+
+}
