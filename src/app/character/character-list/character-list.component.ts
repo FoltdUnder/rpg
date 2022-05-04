ef8e87bd84abd2e154df7a2efeaf4c5de36f5374
foltdUnder <twinker7130@gmail.com>
2022-05-04 18:55:53 +0300

    feat(list): add "create" button

--- src/app/character/character-list/character-list.component.ts
+++ src/app/character/character-list/character-list.component.ts
@@ -3,6 +3,7 @@ import {select, Store} from '@ngrx/store';
 import {selectCharacterList} from './character-list.selectors';
 import {Observable} from 'rxjs';
 import {Character} from '../character.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-character-list',
@@ -11,10 +12,14 @@ import {Character} from '../character.model';
 })
 export class CharacterListComponent implements OnInit {
   characterList$ = new Observable<Character[]>();
-  constructor(private store: Store) { }
+  constructor(private store: Store,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.characterList$ = this.store.pipe(select(selectCharacterList));
   }
 
+  createCharacter() {
+    this.router.navigateByUrl('create');
+  }
 }
